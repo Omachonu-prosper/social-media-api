01ac4ae52b437150b99a73005c64078772be1077
Omachonu Prosper <omachonucodes@gmail.com>
2024-04-12 07:12:03 +0100

    Functionality to create text only posts

--- api/user_actions.py
+++ api/user_actions.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request, session
 from api.middlewares import api_key_required, login_required
-from utils.db import users
+from datetime import datetime
+from uuid import uuid4
+from utils.db import users, posts
 from utils.request_parser import Parser
 
 """All user interactions such as
@@ -16,4 +18,27 @@ actions = Blueprint('actions', __name__)
 @api_key_required
 @login_required
 def create_post():
-    return 'ok'
+    parser = Parser(request.json)
+    parser.add_item('text', True, 'text field can not be empty')
+    if not parser.valid:
+        return parser.generate_errors(
+            'bad request: check the errors field for details on the error',
+            400
+        )
+    
+    data = parser.get_items()
+    text = data['text']
+    user_id = session['user_id']
+    post_id = str(uuid4())
+    now = datetime.now()
+    posts.insert_one({
+        'text': text,
+        'uid': user_id,
+        'created_at': now,
+        'pid': post_id
+    })
+
+    return jsonify({
+        'message': 'post created successfully',
+        'status': True
+    }), 200
--- utils/db.py
+++ utils/db.py
@@ -4,3 +4,4 @@ from config import Config
 client = MongoClient(Config.DB_URI)
 db = client['Social_Media_API']
 users = db['Users'] # Users collection
+posts = db['Posts'] # Posts collection
